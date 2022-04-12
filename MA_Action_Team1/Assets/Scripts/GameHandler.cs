ae264dd0ee6ab40d07c24862b7593a6b8676f404
Kan322 <ksasaki759@massart.edu>
2022-04-12 10:16:08 -0400

    Created Game Scene Canvas GameHandler Prefab

--- MA_Action_Team1/Assets/Scripts/GameHandler.cs
+++ MA_Action_Team1/Assets/Scripts/GameHandler.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
 
 public class GameHandler : MonoBehaviour
 {
@@ -22,6 +23,23 @@ public class GameHandler : MonoBehaviour
 
     private string sceneName;
 
+    public static bool GameisPaused = false;
+    public GameObject pauseMenuUI;
+    public AudioMixer mixer;
+    public static float volumeLevel = 1.0f;
+    private Slider sliderVolumeCtrl;
+
+    void Awake()
+    {
+        SetLevel(volumeLevel);
+        GameObject sliderTemp = GameObject.FindWithTag("PauseMenuSlider");
+        if (sliderTemp != null)
+        {
+            sliderVolumeCtrl = sliderTemp.GetComponent<Slider>();
+            sliderVolumeCtrl.value = volumeLevel;
+        }
+    }
+
     void Start()
     {
         player = GameObject.FindWithTag("Player");
@@ -30,6 +48,44 @@ public class GameHandler : MonoBehaviour
         playerHealth = StartPlayerHealth;
         //}
         updateStatsDisplay();
+        pauseMenuUI.SetActive(false);
+        GameisPaused = false;
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameisPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameisPaused = true;
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameisPaused = false;
+    }
+
+    public void SetLevel(float sliderValue)
+    {
+        mixer.SetFloat("MusicVolume", Mathf.Log10(sliderValue) * 20);
+        volumeLevel = sliderValue;
     }
 
     public void playerGetTokens(int newTokens)

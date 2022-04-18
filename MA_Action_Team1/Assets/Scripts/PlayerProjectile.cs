29e77f794543544830fc4fc1e210d2a4b5061ef8
rnsasson <99921295+rnsasson@users.noreply.github.com>
2022-04-17 23:34:41 -0400

    projectile_attempt
    tried to start, but it's barely anything

---
+++ MA_Action_Team1/Assets/Scripts/PlayerProjectile.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+
+public class PlayerProjectile : MonoBehaviour{
+
+      public int damage = 1;
+      public GameObject hitEffectAnim;
+      public float SelfDestructTime = 2.0f;
+
+      void Start(){
+           StartCoroutine(selfDestruct());
+      }
+
+      //if the bullet hits a collider, play the explosion animation, then destroy the effect and the bullet
+      void OnTriggerEnter2D(Collider2D other){
+            if (other.gameObject.layer == LayerMask.NameToLayer("Enemies")) {
+                  gameHandlerObj.playerGetHit(damage);
+            }
+           if (other.gameObject.tag != "Player") {
+                  GameObject animEffect = Instantiate (hitEffectAnim, transform.position, Quaternion.identity);
+                  Destroy (animEffect, 0.5f);
+                  Destroy (gameObject);
+            }
+      }
+
+      IEnumerator selfDestruct(){
+            yield return new WaitForSeconds(SelfDestructTime);
+            Destroy (gameObject);
+      }
+}

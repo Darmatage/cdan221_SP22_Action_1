f3927a5d71d46074aae2b74a8d6e7b2449de2de1
JasonWiser <JasonWiserArt@gmail.com>
2022-05-06 16:10:36 -0400

    Player Lightning is enabled

--- MA_Action_Team1/Assets/Scripts/PlayerAttackMelee.cs
+++ MA_Action_Team1/Assets/Scripts/PlayerAttackMelee.cs
@@ -4,41 +4,95 @@ using UnityEngine;
 
 public class PlayerAttackMelee : MonoBehaviour{
 
-      public Animator animator;
-      public Transform attackPt;
-      public float attackRange = 0.5f;
-      public float attackRate = 2f;
-      private float nextAttackTime = 0f;
-      public int attackDamage = 20;
-      public LayerMask enemyLayers;
+	public Animator anim;
+	  
+	//attack points for hit range
+	public Transform attackPt;
+	public Transform attackPt_Front;
+	public Transform attackPt_Up;
+	public Transform attackPt_Down;
+	  
+	public float attackRange = 2.5f;
+	public float attackRate = 1f; //the smaller the fraction, the slower the cooldown
+	public float lightningRate = 0.3f; //the smaller the fraction, the slower the cooldown
+	private  float nextAttackTime = 0f;
+	private float nextLightningTime = 0f;
+	public int attackDamage = 20;
+	public LayerMask enemyLayers;
 
-      void Start(){
-           animator = gameObject.GetComponentInChildren<Animator>();
-      }
+	public bool isTail = true;
+	public bool isLightning = false;
 
-      void Update(){
-           if (Time.time >= nextAttackTime){
-                  //if (Input.GetKeyDown(KeyCode.Space))
-                 if (Input.GetAxis("AttackMelee") > 0){
-                        Attack();
-                        nextAttackTime = Time.time + 1f / attackRate;
-                  }
-            }
-      }
+	public bool isFront = true;
+	public bool isUp = false;
+	public bool isDown = false;
 
-      void Attack(){
-            animator.SetTrigger ("tailswing");
-            Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPt.position, attackRange, enemyLayers);
+	public GameObject LightningFront;
+	public GameObject LightningUp;
+	public GameObject LightningDown;
 
-            foreach(Collider2D enemy in hitEnemies){
-                  Debug.Log("We hit " + enemy.name);
-                  enemy.GetComponent<EnemyMeleeDamage>().TakeDamage(attackDamage);
-            }
-      }
+	void Start(){
+		anim = gameObject.GetComponentInChildren<Animator>();
+		LightningFront.SetActive(false);
+		LightningUp.SetActive(false);
+		LightningDown.SetActive(false);
+	}
 
-      //NOTE: to help see the attack sphere in editor:
-      void OnDrawGizmosSelected(){
-           if (attackPt == null) {return;}
-            Gizmos.DrawWireSphere(attackPt.position, attackRange);
-      }
+	void Update(){
+		if (Time.time >= nextAttackTime){
+			//if (Input.GetKeyDown(KeyCode.Space))
+			if (Input.GetAxis("AttackMelee") > 0){
+				isTail = true;
+				isLightning = false;
+				Attack();
+				nextAttackTime = Time.time + 1f / attackRate;
+			}
+		}
+		
+		if (Time.time >= nextLightningTime){		
+			if (Input.GetAxis("AttackLightning") > 0){
+				isTail = false;
+				isLightning = true;
+				Attack();
+				nextLightningTime = Time.time + 1f / lightningRate;
+				Debug.Log("Time.time = " + Time.time + "nextLightningTime" + nextLightningTime);
+			}
+		}		
+	}
+
+	void Attack(){
+		if (isTail){
+			anim.SetTrigger ("tailswing"); attackPt = attackPt_Front;
+		} else if (isLightning){
+			anim.SetTrigger ("ShootLightning");
+			if (isUp){attackPt = LightningUp.transform; LightningUp.SetActive(true);}
+			else if (isDown){attackPt = LightningDown.transform; LightningDown.SetActive(true);}
+			else {attackPt = LightningFront.transform; LightningFront.SetActive(true);}
+			StopCoroutine(TurnOffLightning());
+			StartCoroutine(TurnOffLightning());
+		}
+		
+		Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPt.position, attackRange, enemyLayers);
+
+		foreach(Collider2D enemy in hitEnemies){
+			Debug.Log("We hit " + enemy.name);
+			enemy.GetComponent<EnemyMeleeDamage>().TakeDamage(attackDamage);
+		}
+	}
+
+	IEnumerator TurnOffLightning(){
+		yield return new WaitForSeconds(1f);
+		LightningFront.SetActive(false);
+		LightningUp.SetActive(false);
+		LightningDown.SetActive(false);
+	}
+
+
+	//NOTE: to help see the attack sphere in editor:
+	void OnDrawGizmosSelected(){
+		Gizmos.DrawWireSphere(LightningFront.transform.position, attackRange);
+		Gizmos.DrawWireSphere(LightningUp.transform.position, attackRange);
+		Gizmos.DrawWireSphere(LightningDown.transform.position, attackRange);
+	}
+	
 }
